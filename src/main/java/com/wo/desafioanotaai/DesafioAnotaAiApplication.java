3505a65fc970902739eb78dffa74235569105843
wanderson648 <testepedroca@gmail.com>
2024-01-20 22:58:03 -0300

    configuracao database mongoDB

--- src/main/java/com/wo/desafioanotaai/DesafioAnotaAiApplication.java
+++ src/main/java/com/wo/desafioanotaai/DesafioAnotaAiApplication.java
@@ -2,8 +2,10 @@ package com.wo.desafioanotaai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class DesafioAnotaAiApplication {
 
 	public static void main(String[] args) {
