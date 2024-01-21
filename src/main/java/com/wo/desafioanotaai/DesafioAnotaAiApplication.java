79b1d968a54b2526babee804064b0e96ce5ea2b9
wanderson648 <testepedroca@gmail.com>
2024-01-20 22:50:15 -0300

    configura projeto

---
+++ src/main/java/com/wo/desafioanotaai/DesafioAnotaAiApplication.java
@@ -0,0 +1,13 @@
+package com.wo.desafioanotaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAnotaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAnotaAiApplication.class, args);
+	}
+
+}
